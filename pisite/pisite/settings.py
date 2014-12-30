1e2d23d2a6332b3d6cb82118240c36689b7db5b4
sizlo <t.brier93@gmail.com>
2014-12-30 17:23:59 +0000

    Add a homepage

--- pisite/pisite/settings.py
+++ pisite/pisite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'logs',
+    'home',
 )
 
 MIDDLEWARE_CLASSES = (
--- pisite/pisite/urls.py
+++ pisite/pisite/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from home import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pisite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^logs/', include('logs.urls', namespace="logs")),
     url(r'^admin/', include(admin.site.urls)),
+
+    # Home page
+    url(r'^$', views.index, name="home"),
 )
