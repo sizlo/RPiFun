5d71215645683a059a51407a3768054c9ea77406
sizlo <t.brier93@gmail.com>
2015-01-05 19:17:50 +0000

    Add to the label that 0 lines will result in the entire file being downloaded

--- pisite/logs/forms.py
+++ pisite/logs/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from logs.models import Log
 
 class LineCountForm(forms.Form):
-  linesToFetch = forms.IntegerField(label="Number of lines to show", min_value=0, initial=Log.defaultLinesToShow)
+  linesToFetch = forms.IntegerField(label="Number of lines to show (0 for all)", min_value=0, initial=Log.defaultLinesToShow)
