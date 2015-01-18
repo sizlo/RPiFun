2e95950a18d9bdfb080dffa9a854521abac0c7c3
sizlo <t.brier93@gmail.com>
2015-01-18 19:09:41 +0000

    Use the full path of the config file

--- pisite/motionSound/views.py
+++ pisite/motionSound/views.py
@@ -15,7 +15,7 @@ def index(request):
     # The toggle sound button was clicked, edit the config file
     configFile = get_object_or_404(TextFile, name="config")
     config = ConfigParser.RawConfigParser()
-    config.read(configFile.filePath)
+    config.read(settings.BASE_DIR + "/" + configFile.filePath)
     soundDisabled = config.getboolean("misc", "sounddisabled")
     soundDisabled = not soundDisabled
     config.set("misc", "sounddisabled", soundDisabled)
